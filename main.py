8b53febd2f4b085d478b4a07c83d80d3e4009827
Osip <osypenko.k@gmail.com>
2022-11-29 17:00:10 +0200

    Imported modules, created mesage after start, prepare func and file for string info

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def get_number():
-    import random
-    return random.randint(1,100)
+import telebot
+from flask import Flask, request
+import os
 
-print(get_number())
+app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def message_start(message):
+    bot.send_message(message.chat.id, "Nice to see you in Morty and Osip Bot. Type String to see Python string methods")
+
+@bot.message_handler(commands=['String'])
+def message_string(message):
+    pass
