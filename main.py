3306c6d3e8faad75681ff246561916f1a3539464
Osip <osypenko.k@gmail.com>
2022-11-29 19:58:46 +0200

    Added py_help.txt, telegram buttons, start message

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ app = Flask(__name__)
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def message_start(message):
-    bot.send_message(message.chat.id, "Nice to see you in Morty and Osip Bot. Type String to see Python string methods")
+    bot.send_message(message.chat.id, "Nice to see you in Morty and Osip Bot. Type \"Help\" for some PyHelp")
 
-@bot.message_handler(commands=['String'])
-def message_string(message):
-    pass
+
+@bot.message_handler(commands=['Help'])
+def message_help(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    with open('py_help.txt') as file:
+        helper = [item.split(',') for item in file]
+
+        for title, link in helper:
+            link_button = telebot.types.InlineKeyboardButton(text=title.strip(), url=link.strip())
+            keyboard.add(link_button)
+
+        bot.send_message(message.chat.id, "Python Built-In-Types", reply_markup=keyboard)
